a2fcce50781e444392e84d0e4af8adb8a8ec712d
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-04-29 18:44:11 +0200

    return single product selection

--- php/tests/BundleConfiguratorTest.php
+++ php/tests/BundleConfiguratorTest.php
@@ -8,10 +8,17 @@ use PHPUnit\Framework\TestCase;
 
 final class BundleConfiguratorTest extends TestCase
 {
-    public function test_change_me(): void
+    public function test_product_p1_returns_p1(): void
     {
         $bundleConfigurator = new BundleConfigurator();
 
         self::assertSame('P1', $bundleConfigurator->select('P1'));
+    }
+
+    public function test_p2_returns_p2(): void
+    {
+        $bundleConfigurator = new BundleConfigurator();
+
+        self::assertSame('P2', $bundleConfigurator->select('P2'));
     }
 }
