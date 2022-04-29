b0101d9d6cf9964f021b2338d51f039d919c9544
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-04-29 19:44:50 +0200

    refactor simplify test

--- php/tests/BundleConfiguratorTest.php
+++ php/tests/BundleConfiguratorTest.php
@@ -8,24 +8,11 @@ use PHPUnit\Framework\TestCase;
 
 final class BundleConfiguratorTest extends TestCase
 {
-    public function test_product_p1_returns_p1(): void
+    public function test_single_product_returns_same_product(): void
     {
-        $bundles = [
-            'P1,P2' => 'B1',
-            'P2,P1' => 'B1',
-            'P1,P4' => 'B2',
-        ];
-
-        $bundleConfigurator = new BundleConfigurator($bundles);
+        $bundleConfigurator = new BundleConfigurator([]);
 
         self::assertSame('P1', $bundleConfigurator->select('P1'));
-    }
-
-    public function test_p2_returns_p2(): void
-    {
-        $bundleConfigurator = new BundleConfigurator();
-
-        self::assertSame('P2', $bundleConfigurator->select('P2'));
     }
 
     public function test_p1_p2_then_b1(): void
