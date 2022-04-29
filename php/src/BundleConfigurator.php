12176171a48f05e8a5a8aa3a065be2a951eb59c8
Jose Maria Valera Reales <jose.valera@teufel.de>
2022-04-29 19:33:20 +0200

    Remove else!

--- php/src/BundleConfigurator.php
+++ php/src/BundleConfigurator.php
@@ -5,19 +5,19 @@ namespace Kata;
 
 final class BundleConfigurator
 {
-    private $bundles;
+    private array $bundles;
 
     public function __construct(array $bundles = [])
     {
         if (empty($bundles)) {
-            $this->bundles = [
+            $bundles = [
                 'P1,P2' => 'B1',
                 'P2,P1' => 'B1',
                 'P1,P4' => 'B2',
             ];
-        } else {
-            $this->bundles = $bundles;
         }
+
+        $this->bundles = $bundles;
     }
 
     public function select(string $productNames): string
